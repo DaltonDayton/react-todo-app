cc3260a76aa97db61362293df0dfb0519f8952da
Dalton Dayton <daltondayton1@gmail.com>
2023-06-09 11:19:21 -0600

    add AppHeader, Button, and getClasses util

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+function AppHeader() {
+  return (
+    <div>
+      <h1>Hello from AppHeader</h1>
+      <Button variant="primary">Click Me</Button>
+    </div>
+  );
+}
+
+export default AppHeader;
